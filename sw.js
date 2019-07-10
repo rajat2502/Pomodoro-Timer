764d226d634f72a59f0e239b4deb3bd5bcb3995a
abc <rajat2502@users.noreply.github.com>
2019-07-11 00:44:02 +0530

    pwa attempt 4

--- sw.js
+++ sw.js
@@ -1,88 +1,132 @@
 
-// var cacheName = 'hello-pwa';
-// var filesToCache = [
-//   '/',
-//   '/index.html',
-//   '/style.css',
-//   '/js/app.js'
-// ];
+// // var cacheName = 'hello-pwa';
+// // var filesToCache = [
+// //   '/',
+// //   '/index.html',
+// //   '/style.css',
+// //   '/js/app.js'
+// // ];
 
-// /* Start the service worker and cache all of the app's content */
-// self.addEventListener('install', function(e) {
-//   e.waitUntil(
-//     caches.open(cacheName).then(function(cache) {
-//       return cache.addAll(filesToCache);
-//     })
-//   );
-// });
+// // /* Start the service worker and cache all of the app's content */
+// // self.addEventListener('install', function(e) {
+// //   e.waitUntil(
+// //     caches.open(cacheName).then(function(cache) {
+// //       return cache.addAll(filesToCache);
+// //     })
+// //   );
+// // });
 
-// /* Serve cached content when offline */
-// self.addEventListener('fetch', function(e) {
-//   e.respondWith(
-//     caches.match(e.request).then(function(response) {
-//       return response || fetch(e.request);
-//     })
-//   );
-// });
+// // /* Serve cached content when offline */
+// // self.addEventListener('fetch', function(e) {
+// //   e.respondWith(
+// //     caches.match(e.request).then(function(response) {
+// //       return response || fetch(e.request);
+// //     })
+// //   );
+// // });
 
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js");
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.0.0/workbox-sw.js");
 
-if (workbox) {
-    console.log("Yay! Workbox is loaded !");
-    workbox.precaching.precacheAndRoute([]);
+// if (workbox) {
+//     console.log("Yay! Workbox is loaded !");
+//     workbox.precaching.precacheAndRoute([]);
 
-    /*  cache images in the e.g others folder; edit to other folders you got
-   and config in the sw-config.js file
-    */
-    workbox.routing.registerRoute(
-        /(.*)others(.*)\.(?:png|gif|jpg)/,
-        new workbox.strategies.CacheFirst({
-            cacheName: "images",
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 50,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                })
-            ]
-        })
-    );
-    /* Make your JS and CSS âš¡ fast by returning the assets from the cache,
-  while making sure they are updated in the background for the next use.
-  */
-    workbox.routing.registerRoute(
-    // cache js, css, scc files
-        /.*\.(?:css|js|scss|)/,
-        // use cache but update in the background ASAP
-        new workbox.strategies.StaleWhileRevalidate({
-            // use a custom cache name
-            cacheName: "assets",
-        })
-    );
+//     /*  cache images in the e.g others folder; edit to other folders you got
+//    and config in the sw-config.js file
+//     */
+//     workbox.routing.registerRoute(
+//         /(.*)others(.*)\.(?:png|gif|jpg)/,
+//         new workbox.strategies.CacheFirst({
+//             cacheName: "images",
+//             plugins: [
+//                 new workbox.expiration.Plugin({
+//                     maxEntries: 50,
+//                     maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//                 })
+//             ]
+//         })
+//     );
+//     /* Make your JS and CSS âš¡ fast by returning the assets from the cache,
+//   while making sure they are updated in the background for the next use.
+//   */
+//     workbox.routing.registerRoute(
+//     // cache js, css, scc files
+//         /.*\.(?:css|js|scss|)/,
+//         // use cache but update in the background ASAP
+//         new workbox.strategies.StaleWhileRevalidate({
+//             // use a custom cache name
+//             cacheName: "assets",
+//         })
+//     );
 
-    // cache google fonts
-    workbox.routing.registerRoute(
-        new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
-        new workbox.strategies.CacheFirst({
-            cacheName: "google-fonts",
-            plugins: [
-                new workbox.cacheableResponse.Plugin({
-                    statuses: [0, 200],
-                }),
-            ],
-        })
-    );
+//     // cache google fonts
+//     workbox.routing.registerRoute(
+//         new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+//         new workbox.strategies.CacheFirst({
+//             cacheName: "google-fonts",
+//             plugins: [
+//                 new workbox.cacheableResponse.Plugin({
+//                     statuses: [0, 200],
+//                 }),
+//             ],
+//         })
+//     );
 
-    // add offline analytics
-    workbox.googleAnalytics.initialize();
+//     // add offline analytics
+//     workbox.googleAnalytics.initialize();
 
-    /* Install a new service worker and have it update
-and control a web page as soon as possible
-*/
+//     /* Install a new service worker and have it update
+// and control a web page as soon as possible
+// */
 
-    workbox.core.skipWaiting();
-    workbox.core.clientsClaim();
+//     workbox.core.skipWaiting();
+//     workbox.core.clientsClaim();
 
-} else {
-    console.log("Oops! Workbox didn't load ðŸ‘º");
-}
+// } else {
+//     console.log("Oops! Workbox didn't load ðŸ‘º");
+// }
+
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/js/app.js',
+  '/audio/Tick.mp3',
+  'audio/Loud-alarm-clock-sound.mp3'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+  // CODELAB: Remove previous cached data from disk.
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== cacheName && key !== DATA_CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+
