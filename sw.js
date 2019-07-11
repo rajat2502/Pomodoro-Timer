91fab0de9e9b83599aefa4a58f13831fd7343557
abc <rajat2502@users.noreply.github.com>
2019-07-11 11:24:19 +0530

    added code to cache audios

--- sw.js
+++ sw.js
@@ -73,6 +73,20 @@ if (workbox) {
         })
     );
 
+    workbox.routing.registerRoute(
+      // Custom `matchCallback` function
+      ({event}) => event.request.destination === 'audio',
+      new workbox.strategies.CacheFirst({
+        cacheName: 'audio',
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
+      })
+    );
+
     // add offline analytics
     workbox.googleAnalytics.initialize();
 
